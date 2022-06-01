f35468829eb544ef0c19417036ec16386f90fea3
Shaun Mitchell <shaunmitchell@google.com>
2022-06-01 21:42:50 +0000

    Fixed auth warning on kubectl blocking call

--- cli/cmd/create.go
+++ cli/cmd/create.go
@@ -178,6 +178,8 @@ var createCmd = &cobra.Command{
 				}
 
 				saveStep("network")
+			} else {
+				saveStep("network")
 			}
 		}
 
@@ -204,6 +206,8 @@ var createCmd = &cobra.Command{
 				}
 
 				saveStep("subnet")
+			} else { 
+				saveStep("subnet")
 			}
 		}
 
@@ -224,7 +228,15 @@ var createCmd = &cobra.Command{
 
 		// Adding service account to the owners role
 		if !createSteps.stepExists("add-policy") {
-			cmdArgs := []string{"get", "ConfigConnectorContext", "-n", "config-control", "-o", "jsonpath='{.items[0].spec.googleServiceAccount}'"}
+			cmdArgs := []string{"container", "clusters", "get-credentials", "krmapihost-" + args[0], "--region=" + region, "--project=" + project}
+
+			_, err := utils.CallCommand(gcloud, cmdArgs, false)
+
+			if err != nil {
+				log.Fatal().Err(err).Msg("Unable to get configconnectorcontext")
+			}
+
+			cmdArgs = []string{"get", "ConfigConnectorContext", "-n", "config-control", "-o", "jsonpath='{.items[0].spec.googleServiceAccount}'"}
 
 			ret, err := utils.CallCommand("kubectl", cmdArgs, false)
 
@@ -232,7 +244,7 @@ var createCmd = &cobra.Command{
 				log.Fatal().Err(err).Msg("Unable to get configconnectorcontext")
 			}
 
-			sa := strings.Replace(string(ret), "'", "", 2)
+			sa := strings.Replace(strings.Split(strings.Split(string(ret), "@")[0], "'")[1] + "@" + strings.Split(string(ret), "@")[1], "'", "",1)
 
 			cmdArgs = []string{"projects", "add-iam-policy-binding", project, `--member=serviceAccount:` + sa, `--role=roles/owner`, `--condition=None`}
 
