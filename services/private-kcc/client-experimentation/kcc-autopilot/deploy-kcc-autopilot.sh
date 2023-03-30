f5134130a3c62d3145682c2aec8bf72324c7305f
stjeanl1 <luc.st-jean@ssc-spc.gc.ca>
2023-03-30 16:00:16 -0400

    Fix subnet secondary ranges

--- services/private-kcc/client-experimentation/kcc-autopilot/deploy-kcc-autopilot.sh
+++ services/private-kcc/client-experimentation/kcc-autopilot/deploy-kcc-autopilot.sh
@@ -87,8 +87,6 @@ gcloud compute firewall-rules create allow-egress-azure --action ALLOW --rules t
 gcloud compute firewall-rules create allow-egress-github --action ALLOW --rules tcp:22,tcp:443 --destination-ranges 192.30.252.0/22,185.199.108.0/22,140.82.112.0/20,143.55.64.0/20,20.201.28.151/32,20.205.243.166/32,102.133.202.242/32,20.248.137.48/32,20.207.73.82/32,20.27.177.113/32,20.200.245.247/32,20.233.54.53/32,20.201.28.152/32,20.205.243.160/32,102.133.202.246/32,20.248.137.50/32,20.207.73.83/32,20.27.177.118/32,20.200.245.248/32,20.233.54.52/32 --direction EGRESS --priority 5001 --network "$NETWORK" --enable-logging
 
 # Allow egress to internal, peered vpc and secondary ranges
-# 172.16.0.0/28 - KCC Autopilot Cluster
-# 172.16.0.32/28 - GKE Cluster
 gcloud compute firewall-rules create allow-egress-internal --action ALLOW --rules=all --destination-ranges 192.168.0.0/16,"$MASTER_IPV4_CIDR_BLOCK",172.16.0.128/28,172.16.0.32/28,10.0.0.0/8 --direction EGRESS --priority 1000 --network "$NETWORK" --enable-logging
 
 # Deny egress to internet
