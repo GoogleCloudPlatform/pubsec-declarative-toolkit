4cc193b6f927a8e357a200510d50417417b15fdf
Michael O'Brien <michael@obrienlabs.org>
2023-10-20 10:37:13 -0400

    #446 - cluster/lz delete added (#569)
    * #446 - add vars.sh options script
    
    * #446 - add setup.sh script
    
    * #446 - vars.sh for cluster/lz delete
    
    * #446 - cluster/lz delete added

--- solutions/vars.sh
+++ solutions/vars.sh
@@ -14,9 +14,19 @@
 # limitations under the License.
 
 CIDR_KCC_VPC=192.168.0.0/16
-REGION=northamerica-northeast1
+#REGION=northamerica-northeast1
 # used for vpc, subnet, KCC cluster
-PREFIX=oi2
-CLUSTER=kcc-oi2
+PREFIX=oi3
+#CLUSTER=kcc-oi2
 KCC_PROJECT_NAME=kcc-oi
 public_endpoint_opt=true
+
+# from gcp-tools kcc.env
+CLUSTER=kcc-oi3
+REGION=northamerica-northeast1
+#PROJECT_ID=kcc-oi2-cluster
+LZ_FOLDER_NAME=kcc-lz-20231020c
+NETWORK=kcc-oi3-vpc
+SUBNET=kcc-oi3-sn
+#ROOT_FOLDER_ID=96269513997
+#BILLING_ID=014479-806359-2F5F85
